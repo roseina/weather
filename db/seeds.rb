bec76cfdcc2aaf5a2817d9f7a2cde45fa15db64d
ccr <sis.ccr@gmail.com>
2017-06-14 20:49:42 +0545

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ l.recordings.create(temp: "15", status: "cold")
 l.recordings.create(temp: "20", status: "mild")
 l.recordings.create(temp: "25", status: "hot")
 l.recordings.create(temp: "30", status: "very hot")
+
+statuses = ["cold", "hot", "very hot"]
+
+location_names = ["mahalaxmisthan", "naikap"]
+
+5.times do
+  # we can sample througn the name. test this on your irb its fun trust me :)
+  location = Location.new(name: location_names.sample)
+  temp = rand(100)
+  3.times do
+    location.recordings.build(temp: temp, status: statuses.sample)
+  end
+  location.save!
+end
